6ff78b644d331e77cae1ccd516ef53d130edf36d
shuhi <128314725+Shuhimoon@users.noreply.github.com>
2026-07-08 16:05:46 +0800

    Add files via upload

---
+++ 2015/adventofcode/Day1_1/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"gopkg.in/yaml.v2"
+	"os"
+	"fmt"
+)
+
+type Config struct {
+	FilePath string `yaml:"file_path"`
+}
+
+var currentConfig Config
+
+func init() {
+	loadConfig()
+}
+
+func loadConfig() {
+	paths := []string{"config.yaml", "config/config.yaml", "../config.yaml"}
+	loaded := false
+
+	for _, p := range paths {
+		data, err := os.ReadFile(p)
+		if err == nil {
+			if err := yaml.Unmarshal(data, &currentConfig); err == nil {
+				fmt.Println("成功載入設定檔案: ", p)
+				loaded = true
+				return
+			}
+		}
+
+	}
+
+	if loaded == false {
+		panic("嚴重錯誤：找不到 config.yaml 或 YAML 格式錯誤！請確認檔案存在且格式正確。")
+	}
+}
+
+
+func GetConfig() Config {
+	return currentConfig
+}
